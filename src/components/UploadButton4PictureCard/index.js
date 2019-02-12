a9271e1e0aacd7a71890469c416341b932fe8f34
苏震 <leslie43@sina.com>
2019-02-12 12:29:10 +0800

    upload demo
    upload demo

---
+++ src/components/UploadButton4PictureCard/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Icon } from 'antd';
+export default class UploadButton4PictureCard extends Component {
+    render() {
+        return (
+            <div>
+                <Icon type="plus" />
+                <div className="ant-upload-text">Upload.t</div>
+            </div>
+        )
+    }
+}
--- src/pages/Antd/UploadDemo.js
+++ src/pages/Antd/UploadDemo.js
@@ -1,7 +1,65 @@
-import React, { Component } from 'react';
-import { Form, TimePicker, Upload, Icon, Button, Switch, message, Divider } from 'antd';
+import React, { Component, PureComponent } from 'react';
+import { Form, TimePicker, Upload, Icon, Modal, Button, Switch, message, Divider } from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+import reqwest from 'reqwest'
+import UploadButton4PictureCard from '@/components/UploadButton4PictureCard';
 import moment from 'moment';
+import styles from './LayoutDemo.less';
+const Dragger = Upload.Dragger;
+
+// -------------------- for Dragger
+const props3 = {
+  name: 'file',
+  multiple: true,
+  action: '//jsonplaceholder.typicode.com/posts/',
+  onChange(info) {
+    console.info('In Dragger')
+    console.info(info)
+    // 🚀🚀🚀🚀🚀 info => file + fileList 🚀🚀🚀🚀
+    // file: 最近上传的文件
+    // fileList: 已上传的文件列表
+    const status = info.file.status;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  }
+}
+
+// -------------------- for PictureList
+const fileList = [
+  {
+    uid: '-1',
+    name: 'xxx.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }, {
+    uid: '-2',
+    name: 'yyy.png',
+    status: 'done',
+    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  }];
+
+const props1 = {
+  action: '//jsonplaceholder.typicode.com/posts/',
+  listType: 'picture',
+  defaultFileList: [...fileList],
+};
+
+const props2 = {
+  action: '//jsonplaceholder.typicode.com/posts/',
+  listType: 'picture',
+  defaultFileList: [...fileList],
+  className: styles['ant-upload-list-item'],
+};
+// -------------------- for PictureList
+
 
 const props = {
   name: 'file',
@@ -10,6 +68,35 @@ const props = {
     authorization: 'authorization-text',
   }
 }
+
+const defaultProps = {
+  action: '//jsonplaceholder.typicode.com/posts/',
+  onChange({ file, fileList }) {
+    if (file.status !== 'uploading') {
+      console.info(file, fileList)
+    }
+  },
+  // file list format: uid,name,status,url
+  defaultFileList: [{
+    uid: '1',
+    name: 'xxx.png',
+    status: 'done',
+    response: 'Server Error 500', // custom error message to show
+    url: 'http://www.baidu.com/xxx.png',
+  }, {
+    uid: '2',
+    name: 'yyy.png',
+    status: 'done',
+    url: 'http://www.baidu.com/yyy.png',
+  }, {
+    uid: '3',
+    name: 'zzz.png',
+    status: 'error',
+    response: 'Server Error 500', // custom error message to show
+    url: 'http://www.baidu.com/zzz.png',
+  }]
+}
+
 function getBase64(img, callback) {
   const reader = new FileReader()
   reader.addEventListener('load', () => callback(reader.result))
@@ -30,6 +117,7 @@ class Avatar extends Component {
   state = { loading: false }
 
   handleChange = (info) => {
+    console.info('handleChange')
     console.info(info)
     if (info.file.status === 'uploading') {
       this.setState({ loading: true })
@@ -43,13 +131,9 @@ class Avatar extends Component {
     }
   }
   render() {
-    const uploadButton = (
-      <div>
-        <Icon type={this.state.loading ? 'loading' : 'plus'}></Icon>
-        <div className="antd-upload-text">Upload</div>
-      </div>
-    )
+    // 头像，只显示和操作一个
     const imageUrl = this.state.imageUrl;
+    console.info(imageUrl);
     return (
       <Upload name="avatar"
         listType="picture-card"
@@ -59,8 +143,215 @@ class Avatar extends Component {
         beforeUpload={beforeUpload}
         onChange={this.handleChange}
       >
-        {imageUrl ? <img src={imageUrl} alt="avatar" /> : uploadButton}
+        {imageUrl ? <img style={{ width: 80, height: 60 }} src={imageUrl} alt="avatar" /> : <UploadButton4PictureCard />}
       </Upload>
+    )
+  }
+}
+
+class PictureWall extends Component {
+  state = {
+    previewVisible: false,
+    previewImage: '',
+    fileList: [{
+      uid: '-1',
+      name: 'xxx.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    }]
+  }
+  handleCancel = () => {
+    this.setState({ previewVisible: false })
+  }
+  handlePreview = (file) => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true
+    })
+  }
+
+  // handleChange传递的参数就是当前的fileList
+  handleChange = ({ fileList }) => this.setState({ fileList })
+
+  render() {
+    const { previewVisible, previewImage, fileList } = this.state
+    return (
+      <div className="clearfix">
+        <Upload
+          action="//jsonplaceholder.typicode.com/posts/"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}>
+          {fileList.length >= 5 ? null : <UploadButton4PictureCard />}
+        </Upload>
+        <Modal visible={previewVisible}
+          footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
+    )
+  }
+}
+
+class MyUpload extends Component {
+  state = {
+    fileList: [{
+      uid: '-1',
+      name: 'xxx.png',
+      status: 'done',
+      url: 'http://www.baidu.com/xxx.png',
+    }]
+  }
+
+  handleChange = (info) => {
+    console.info('MyUpload')
+    console.info(info)
+    let fileList = info.fileList
+    // 1. Limit the number of uploaded files
+    // Only to show two recent uploaded files, and old ones will be replaced by the new
+    fileList = fileList.slice(-2)
+    // 2. Read from response and show file link
+    fileList = fileList.map((file) => {
+      if (file.response) {
+        // Component will show file.url as link
+        file.url = file.response.url
+      }
+      return file
+    })
+    // 3. Filter successfully uploaded files according to response from server
+    fileList = fileList.filter((file) => {
+      if (file.response) {
+        return file.response.status === 'success'
+      }
+      return false
+    })
+    this.setState({ fileList })
+  }
+
+  render() {
+    const props = {
+      action: '//jsonplaceholder.typicode.com/posts/',
+      onChange: this.handleChange,
+      multiple: true,
+    }
+    return (
+      <Upload {...props} fileList={this.state.fileList}>
+        <Button><Icon type="upload" /></Button>
+      </Upload>
+    )
+  }
+}
+
+class PictureList extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Upload {...props1}>
+          <Button><Icon type="upload" />upload</Button>
+        </Upload>
+        <br /> <br />
+        <Upload {...props2}>
+          <Button><Icon type="upload" />upload</Button>
+        </Upload>
+      </div>
+    )
+  }
+}
+
+class DraggerDemo extends Component {
+  render() {
+    return (
+      <Dragger {...props3}>
+        <p className="ant-upload-drag-icon">
+          <Icon type="inbox" />
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+      </Dragger>
+    )
+  }
+}
+
+class ManuallyDemo extends Component {
+  state = {
+    fileList: [],
+    uploading: false,
+  }
+
+  handleUpload = () => {
+    const { fileList } = this.state
+    const formData = new FormData()
+    fileList.forEach((file) => {
+      formData.append('files[]', file)
+    })
+    this.setState({
+      uploading: true
+    })
+    // Ajax
+    reqwest({
+      url: '//jsonplaceholder.typicode.com/posts/',
+      method: 'post',
+      processData: false,
+      data: formData,
+      // success
+      success: () => {
+        this.setState({
+          fileList: [],
+          uploading: false
+        })
+        message.success('Upload successfully.')
+      },
+      // error
+      error: () => {
+        this.setState({
+          uploading: false
+        })
+        message.error('Upload failed.')
+      }
+    })
+  }
+
+  render() {
+    const { uploading, fileList } = this.state;
+    const props = {
+      // remove
+      onRemove: (file) => {
+        this.setState((state) => {
+          const index = state.fileList.indexOf(file)
+          const newFileList = state.fileList.slice()
+          newFileList.splice(index, 1)
+          return {
+            fileList: newFileList
+          }
+        })
+      },
+      //before upload
+      beforeUpload: (file) => {
+        console.info(file)
+        this.setState(state => ({
+          fileList: [...state.fileList, file]
+        }))
+        return false;
+      },
+      fileList
+    }
+    return (
+      <div>
+        <Upload listType="picture" {...props}>
+          <Button>
+            <Icon type="upload" /> Select File
+          </Button>
+        </Upload>
+        <Button type="primary"
+          onClick={this.handleUpload}
+          disabled={fileList.length === 0}
+          loading={uploading}
+          style={{ marginTop: 16 }}
+        >
+          {uploading ? 'Uploading' : 'Start upload'}
+        </Button>
+      </div >
     )
   }
 }
@@ -70,7 +361,7 @@ class UploadDemo extends Component {
 
   onChange = (info) => {
     if (info.file.status !== 'uploading') {
-      console.info(info.file, info.fileList);
+      // console.info(info.file, info.fileList);
     }
     if (info.file.status === 'done') {
       message.success(`${info.file.name} file uploaded successfully.`)
@@ -86,12 +377,28 @@ class UploadDemo extends Component {
           <Button><Icon type="upload" />Click to upload</Button>
         </Upload>
         <Divider />
+
         <Avatar />
         <Divider />
+
         <Upload action="//jsonplaceholder.typicode.com/posts/" directory>
           <Button>
             <Icon type="upload" /> Upload Directory</Button>
-        </Upload>,
+        </Upload>
+        <Divider />
+        <PictureWall />
+        <Divider>defaultFileList custom error message</Divider>
+        <Upload {...defaultProps}>
+          <Button><Icon type="upload" />upload</Button>
+        </Upload>
+        <Divider />
+        <MyUpload />
+        <Divider />
+        <PictureList />
+        <Divider />
+        <DraggerDemo />
+        <Divider />
+        <ManuallyDemo />
       </PageHeaderWrapper >
     )
   }
